d2497241c548f296e9d0c42e8c8c709b88a2ceb2
Mark Grant <mark.alan.grant@gmail.com>
2016-10-15 06:34:15 -0500

    Cleanup

--- rust/src/btree.rs
+++ rust/src/btree.rs
@@ -3,13 +3,14 @@ use std::mem;
 use std::fmt::Display;
 use std::fmt;
 
+#[derive(Clone)]
 pub struct BTree<K,V> {
     order: usize,
     root: BTreeNode<K,V>
 }
 
+#[derive(Clone)]
 struct BTreeNode<K, V> {
-    order: usize,
     entries: Vec<Entry<K, V>>,
     children: Vec<BTreeNode<K, V>>
 }
@@ -19,6 +20,7 @@ enum InsertionResult<K,V> {
     Split(BTreeNode<K,V>, Entry<K,V>, BTreeNode<K,V>)
 }
 
+#[derive(Clone)]
 struct Entry<K, V> {
     key: K,
     value: V
@@ -32,68 +34,74 @@ impl <K:Display, V: Display> Display for Entry<K,V> {
 
 impl <K: Display, V: Display> Display for BTreeNode<K, V> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-
-        write!(f, "{}", self.stringify(1))
+        write!(f, "{}", self.stringify(0))
     }
 }
 
 impl <K: Display, V: Display> BTreeNode<K, V> {
     fn stringify(&self, num_spaces: i32) -> String {
-        let entries: Vec<_> = self.entries
+        let entries = self.entries
             .iter()
             .map(|ref entry| format!("({},{})", entry.key, entry.value))
-            .collect();
+            .collect::<Vec<_>>()
+            .join(",");
 
 
-        let spaces: Vec<_> = (1..num_spaces)
+        let spaces = (0..num_spaces)
             .map(|x| format!("  "))
-            .collect();
+            .collect::<Vec<_>>()
+            .join("");
 
-        let children: Vec<_> = self.children
+        let children = self.children
             .iter()
             .map(|child| child.stringify(num_spaces + 1))
-            .collect();
+            .collect::<Vec<_>>()
+            .join("");
 
-        format!("{}[{}]\n{}", spaces.join(""), entries.join(","), children.join(""))
+        format!("{}[{}]\n{}", spaces, entries, children)
     }
 }
 
 impl <K: Display, V: Display> Display for BTree<K, V> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.root)
+        write!(f, "order: {}\n{}", self.order, self.root)
     }
 }
 
 
-impl <K: Ord, V: Display> BTree<K,V> {
+impl <K: Ord, V> BTree<K,V> {
     pub fn new(order: usize) -> BTree<K,V> {
         BTree{order: order, root: BTreeNode::new(order)}
     }
 
-    pub fn search(&self, key: K) -> Option<&V> {
+    pub fn search(&self, key: &K) -> Option<&V> {
         self.root.search(key)
     }
 
     pub fn insert(&mut self, key: K, val: V) {
-        match self.root.insert(key, val) {
+        match self.root.insert(key, val, self.order) {
             InsertionResult::Success => {}
             InsertionResult::Split(left,mid,right) => {
-                self.root.entries = vec![mid];
-                self.root.children = vec![left,right];
+                let mut node = BTreeNode::new(self.order);
+                node.entries.push(mid);
+                node.children.push(left);
+                node.children.push(right);
+
+                self.root = node;
             }
         }
     }
 }
 
-impl <K: Ord, V: Display> BTreeNode<K, V> {
+impl <K: Ord, V> BTreeNode<K, V> {
     fn new(order: usize) -> BTreeNode<K, V> {
         let entries = Vec::with_capacity(order - 1);
         let children = Vec::with_capacity(order);
-        BTreeNode{order: order, entries: entries, children: children}
+        BTreeNode{entries: entries, children: children}
     }
 
-    fn search(&self, key: K) -> Option<&V> {
-        match self.insert_index(&key) {
+    fn search(&self, key: &K) -> Option<&V> {
+        match self.insert_index(key) {
             (idx, true) => Some(&self.entries[idx].value),
             (idx, false) => {
                 if self.is_leaf() { None }
@@ -102,47 +110,41 @@ impl <K: Ord, V: Display> BTreeNode<K, V> {
         }
     }
 
-    fn insert(&mut self, key: K, val: V) -> InsertionResult<K, V> {
+    fn insert(&mut self, key: K, val: V, order: usize) -> InsertionResult<K, V> {
         match self.insert_index(&key) {
             (idx, true) => {
                 self.entries[idx].value = val;
                 InsertionResult::Success
             },
             (idx, false) => {
-                // full leaf
-                if self.is_full() && self.is_leaf() {
+                // leaf
+                if self.is_leaf() {
                     let entry = Entry{key: key, value: val};
                     self.entries.insert(idx, entry);
-                    let (left, mid, right) = self.split_overfull_node();
-                    InsertionResult::Split(left, mid, right)
-                }
-                // leaf with space left
-                else if self.is_leaf() {
-                    let entry = Entry{key: key, value: val};
-                    self.entries.insert(idx, entry);
-                    InsertionResult::Success
+                    self.normalize(order)
                 }
                 // non-leaf
                 else {
-                    match self.children[idx].insert(key, val) {
+                    match self.children[idx].insert(key, val, order) {
                         InsertionResult::Success => InsertionResult::Success,
                         InsertionResult::Split(left, mid, right) => {
-                            let full = self.is_full();
-
                             self.entries.insert(idx, mid);
                             self.children[idx] = left;
                             self.children.insert(idx+1, right);
-
-                            if full {
-                                let (left, mid, right) = self.split_overfull_node();
-                                InsertionResult::Split(left, mid, right)
-                            } else {
-                                InsertionResult::Success
-                            }
+                            self.normalize(order)
                         }
                     }
                 }
             }
+        }
+    }
+
+    fn normalize(&mut self, order: usize) -> InsertionResult<K,V> {
+        if self.entries.len() >= order {
+            let (left, mid, right) = self.split_overfull_node(order);
+            InsertionResult::Split(left, mid, right)
+        } else {
+            InsertionResult::Success
         }
     }
 
@@ -156,19 +158,19 @@ impl <K: Ord, V: Display> BTreeNode<K, V> {
         (idx, false)
     }
 
-    fn split_overfull_node(&mut self) -> (BTreeNode<K,V>, Entry<K,V>, BTreeNode<K,V>) {
+    fn split_overfull_node(&mut self, order: usize) -> (BTreeNode<K,V>, Entry<K,V>, BTreeNode<K,V>) {
         let mut entries = Vec::new();
         mem::swap(&mut entries, &mut self.entries);
 
         let mut children = Vec::new();
         mem::swap(&mut children, &mut self.children);
 
-        let splitIdx = self.order / 2;
+        let splitIdx = order / 2;
 
-        let mut left_entries = Vec::with_capacity(self.order - 1);
+        let mut left_entries = Vec::with_capacity(order - 1);
 
         // this is maybe O(n^2)?
-        for idx in (0..splitIdx).rev() {
+        for idx in 0..splitIdx {
             left_entries.push(entries.remove(0));
         }
 
@@ -179,25 +181,19 @@ impl <K: Ord, V: Display> BTreeNode<K, V> {
         if children.len() == 0 {
             left_children = Vec::with_capacity(0);
         } else {
-            left_children = Vec::with_capacity(self.order);
-            for idx in (0..(splitIdx+1)).rev() {
+            left_children = Vec::with_capacity(order);
+            for idx in 0..(splitIdx+1) {
                 left_children.push(children.remove(0));
             }
         }
 
-        let left_node = BTreeNode{order: self.order,
-                                  entries: left_entries,
+        let left_node = BTreeNode{entries: left_entries,
                                   children: left_children};
 
-        let right_node = BTreeNode{order: self.order,
-                                   entries: entries,
+        let right_node = BTreeNode{entries: entries,
                                    children: children};
 
         (left_node, middle_entry, right_node)
-    }
-
-    fn is_full(&self) -> bool {
-        self.entries.len() >= self.order - 1
     }
 
     fn is_leaf(&self) -> bool {
@@ -205,13 +201,26 @@ impl <K: Ord, V: Display> BTreeNode<K, V> {
     }
 }
 
+fn split_vec_right<T: Clone>(vec: &mut Vec<T>, cap: usize, splitIdx: usize, default: T) -> Vec<T> {
+    let mut new_vec = Vec::with_capacity(cap);
+    for idx in splitIdx..vec.len() {
+        let mut val = default.clone();
+        mem::swap(&mut val, &mut vec[splitIdx]);
+        new_vec.push(val);
+    }
+    vec.resize(splitIdx, default);
+    new_vec
+}
+
+// .....
+
 #[cfg(test)]
 mod tests {
     use super::BTree;
 
     #[test]
     fn btree_test() {
-        let mut tree: BTree<i32, i32> = BTree::new(3);
+        let mut tree: BTree<i32, i32> = BTree::new(4);
         for idx in 1..21 {
             tree.insert(idx, idx)
         }
